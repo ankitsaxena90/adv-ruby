506abb0f3d57f7133f62236c193a2cade4ce25a4
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-10 16:57:22 +0530

    interactive program

---
+++ interactive.rb
@@ -0,0 +1,22 @@
+@regex = /^[qQ]$/
+$array = []
+def take_input
+	puts "Enter some Code (q/Q) to quit the program or Press Enter to evaluate"
+	input = gets.chomp
+	$array.push(input.to_s)
+	checkInput(input.to_s)
+end
+
+def checkInput(str)
+	if str.match(@regex)
+		puts "Bye"
+		exit(0)
+	elsif str.length == 0
+		puts eval $array.join
+		$array.clear
+	else
+		take_input()
+	end
+end
+
+take_input()
