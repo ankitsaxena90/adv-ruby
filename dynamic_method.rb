33545b20ec84da9694668533271d58cb5be5989c
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-17 16:47:14 +0530

    dynamic method calling

---
+++ dynamic_method.rb
@@ -0,0 +1,26 @@
+class StringSubclass < String
+
+  def palindrome
+    if self.reverse == self
+      puts "#{ self } is palindrome."
+    else
+      puts "#{ str } is not palindrome."
+    end
+  end
+
+  def inverse
+    puts "Inversed String is: #{self.swapcase}"
+  end
+
+end
+
+
+
+puts "Enter the name of the object to be created"
+object_name = gets.chomp
+obj1 = StringSubclass.new(object_name)
+
+puts "Select any method to be performed 'inverse' / 'palindrome' "
+method_name = gets.chomp.to_s
+
+obj1.send(method_name)
